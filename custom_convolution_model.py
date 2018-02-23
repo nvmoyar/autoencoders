92802a591af89bb927489522d8f0ebc101dc488a
x17 <nohe.veiga@gmail.com>
2018-02-23 14:13:28 +0100

    Build custom deconvolution

--- custom_convolution_model.py
+++ custom_convolution_model.py
@@ -62,18 +62,17 @@ def conv2d_upscale(encoded_tensor, conv_num_outputs, conv_ksize, conv_strides, p
     :param pool_strides: Stride 2-D Tuple for pool
     :padding: text indicating valid or same padding
     : return: A tensor that represents convolution and max pooling of x_tensor
-    """
-   
-    feature_map = x_tensor.get_shape()[3].value
+    """ 
+    
+    # Resize images to size using nearest neighbor interpolation
+    upsample = tf.image.resize_nearest_neighbor(encoded_tensor, (4,4)) # 4x4x4 
     weight = tf.Variable(tf.random_normal([conv_ksize[0], conv_ksize[1], feature_map, conv_num_outputs], mean=0.0, stddev=0.1))
     bias = tf.Variable(tf.zeros([conv_num_outputs]))
-    conv_layer = tf.nn.conv2d(x_tensor, weight, strides=[1, conv_strides[0], conv_strides[1], 1], padding=padding)
+    conv_layer = tf.nn.conv2d(upsample, weight, strides=[1, conv_strides[0], conv_strides[1], 1], padding=padding)
     conv_layer = tf.nn.bias_add(conv_layer, bias)
     conv_layer = tf.nn.relu(conv_layer)
-    conv_layer = tf.nn.max_pool(conv_layer, 
-                                ksize=[1, pool_ksize[0], pool_ksize[1], 1], strides=[1, pool_strides[0], pool_strides[1], 1],
-                                padding=padding)
-            
+    upsample2 = tf.image.resize_nearest_neighbor(conv_layer, depth, ksize=[1, pool_ksize[0], pool_ksize[1], 1])  
+                                            
     return conv_layer
 
 
@@ -91,12 +90,12 @@ def decoding_conv_net(x):
     pool_ksize = (2, 2)
     pool_strides = (2, 2)
             
-    conv_layer = conv2d_maxpool(x, 64, conv_ksize, conv_strides, pool_ksize, pool_strides, 'SAME')
-    conv_layer = conv2d_maxpool(conv_layer, 32, conv_ksize, conv_strides, pool_ksize, pool_strides, 'SAME')
-    conv_layer = conv2d_maxpool(conv_layer, 16, (1, 1), (1, 1), (1, 1), (1, 1), 'SAME')
-    conv_layer = conv2d_maxpool(conv_layer, 8, (1, 1), (1, 1), (1, 1), (1, 1), 'SAME')
-    encoded = conv2d_maxpool(conv_layer, 4, (2, 2), (2, 2), (2, 2), (1, 1), 'SAME')
+    conv_layer = conv2d_upscale(x, 64, conv_ksize, conv_strides, pool_ksize, pool_strides, 'SAME')
+    conv_layer = conv2d_upscale(conv_layer, 32, conv_ksize, conv_strides, pool_ksize, pool_strides, 'SAME')
+    conv_layer = conv2d_upscale(conv_layer, 16, (1, 1), (1, 1), (1, 1), (1, 1), 'SAME')
+    conv_layer = conv2d_upscale(conv_layer, 8, (1, 1), (1, 1), (1, 1), (1, 1), 'SAME')
+    decoded = conv2d_upscale(conv_layer, 4, (2, 2), (2, 2), (2, 2), (1, 1), 'SAME')
     
-    return encoded
+    return decoded
 
 
